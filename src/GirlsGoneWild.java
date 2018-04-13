22a1d2462df4436b273337ce08cf35d9c081a08c
Smytt <parushev.kiril@gmail.com>
2018-04-13 12:07:49 +0300

    girls gone wild done

--- src/GirlsGoneWild.java
+++ src/GirlsGoneWild.java
@@ -5,14 +5,15 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 
 public class GirlsGoneWild {
 
     static void fakeInput() {
-        String input = "5\n" +
-                "baca\n" +
-                "3";
+        String input = "6\n" +
+                "fthavy\n" +
+                "5";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
     }
 
@@ -21,14 +22,55 @@ public class GirlsGoneWild {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int shirts = Integer.parseInt(br.readLine());
         char[] skirts = br.readLine().toCharArray();
-        Arrays.sort(skirts);
         int girls = Integer.parseInt(br.readLine());
         ArrayList<ArrayList<Integer>> shirtsCombinations = new ArrayList<>();
-        ArrayList<ArrayList<Character>> skirtsCombinations = new ArrayList<>();
+        Skirts skirtsCombinations = new Skirts();
 
         combineShirts(shirts, girls, 0, new ArrayList<>(), shirtsCombinations);
-        System.out.println(shirtsCombinations);
+        combineSkirts(skirts, girls, new HashSet<>(), -1, new ArrayList<>(), skirtsCombinations);
 
+        ArrayList<String> output = new ArrayList<>();
+        System.out.println(shirtsCombinations.size() * skirtsCombinations.skirtsCombinations.size());
+
+        for (ArrayList<Integer> oneShirtsCombination: shirtsCombinations){
+            for (ArrayList<Character> oneSkirtCombination: skirtsCombinations.skirtsCombinations) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i< girls; i++) {
+                    sb
+                            .append(oneShirtsCombination.get(i))
+                            .append(oneSkirtCombination.get(i));
+                    if (i != girls - 1) {
+                        sb.append('-');
+                    }
+                }
+                output.add(sb.toString());
+            }
+        }
+
+        Collections.sort(output);
+
+        output.forEach(System.out::println);
+    }
+
+    private static void combineSkirts(char[] skirts, int girls, HashSet<Integer> usedIndexes, int currentSkirtIndex, ArrayList<Character> currentCombination, Skirts skirtsCombinations) {
+        if (currentCombination.size() == girls) {
+            if(!skirtsCombinations.uniqueSkirtsCombinations.contains(currentCombination)) {
+                skirtsCombinations.skirtsCombinations.add(new ArrayList<>(currentCombination));
+                skirtsCombinations.uniqueSkirtsCombinations.add(new ArrayList<>(currentCombination));
+            }
+            return;
+        }
+        for (int i = 0; i < skirts.length; i++) {
+            if(usedIndexes.contains(i)) {
+                continue;
+            }
+            usedIndexes.add(i);
+            char currentSkirt = skirts[i];
+            currentCombination.add(currentSkirt);
+            combineSkirts(skirts, girls, usedIndexes, i, currentCombination, skirtsCombinations);
+            usedIndexes.remove(i);
+            currentCombination.remove(currentCombination.size() - 1);
+        }
     }
 
     private static void combineShirts(int max, int girls, int currentShirt, ArrayList<Integer> currentCombination, ArrayList<ArrayList<Integer>> shirtsCombinations) {
@@ -43,4 +85,9 @@ public class GirlsGoneWild {
             currentCombination.remove(currentCombination.size() - 1);
         }
     }
+}
+
+class Skirts {
+    ArrayList<ArrayList<Character>> skirtsCombinations = new ArrayList<>();
+    HashSet<ArrayList<Character>> uniqueSkirtsCombinations = new HashSet<>();
 }
--- src/Test.java
+++ src/Test.java
@@ -1,113 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 class Test {
     public static void main(String[] args) throws IOException {
-        int[][] field = new int[9][9];
-        readField(field);
+        ArrayList<Integer> one = new ArrayList<>();
+        one.add(3);
+        one.add(5);
+        ArrayList<Integer> two = new ArrayList<>();
+        two.add(3);
+        two.add(5);
 
-        if(solve(field,0,0)) {
-            for (int row = 0; row < 9; row++) {
-                for (int col = 0; col < 9; col++) {
-                    System.out.print(field[row][col]);
-                }
-                System.out.println();
-            }
-        }
-    }
+        HashSet<ArrayList<Integer>> set = new HashSet<>();
 
-    public static boolean isSuitableForDigit(int[][] field, int r, int c, int currDigit) {
+        set.add(one);
+        set.add(two);
 
-        // check in the current row if digit already exists
-        for (int row = 0; row < 9; row++) {
-            if (field[row][c] == currDigit) {
-                return false;
-            }
-        }
-        // check in the current column if digit already exists
-        for (int col = 0; col < 9; col++) {
-            if (field[r][col] == currDigit) {
-                return false;
-            }
-        }
-
-        // check in the current 3x3 box if digit already exists
-        int currentBoxRow = r - r % 3;
-        int currentBoxCol = c - c % 3;
-
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                int currRow = currentBoxRow + row;
-                int currCol = currentBoxCol + col;
-                if (field[currRow][currCol] == currDigit) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean solve(int[][] field,int r,int c) {
-        boolean checkForZero = false;
-        for (int row = r; row < 9; row++) {
-            for (int col = c; col < 9; col++) {
-                if (field[row][col] == 0) {
-                    checkForZero = true;
-                    r = row;
-                    c = col;
-                    break;
-                }
-                c = 0;
-            }
-            if(checkForZero) {
-                break;
-            }
-        }
-
-        // there is no zeros -> the field is filled with digits
-        if (!checkForZero) {
-            return true;
-        }
-
-        for (int currDigit = 1; currDigit <= 9; currDigit++) {
-            // check if the current digit can be put on the current [r][c]
-            if (isSuitableForDigit(field, r, c, currDigit)) {
-                field[r][c] = currDigit;
-
-                if (solve(field,r,c)) {
-                    return true;
-                }
-
-                field[r][c] = 0;
-            }
-        }
-        return false;
-    }
-
-    public static void readField(int[][] field) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        for (int row = 0; row < 9; row++) {
-            String[] line = reader.readLine().replace('-', '0').split("");
-            for (int col = 0; col < 9; col++) {
-                field[row][col] = convert(line[col]);
-            }
-        }
-    }
-
-    private static int convert(String s) {
-        int value = 0;
-        int flag = 1;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '-') {
-                flag = -1;
-            } else {
-                value = value * 10 + (s.charAt(i) - '0');
-            }
-        }
-        if (flag == -1) {
-            return -value;
-        }
-        return value;
+        System.out.println(set);
     }
 }
